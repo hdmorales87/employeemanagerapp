34d416a73f83d62183518f2b03125968a112a1e2
hector morales <hector.morales@logicalsoft.co>
2021-11-11 17:07:48 -0500

    11112021 primer commit

---
+++ src/app/models/Employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    id: number;
+    name: String;
+    email: String;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: String;    
+}
